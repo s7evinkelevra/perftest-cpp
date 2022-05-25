94d1da111b9a551fd8ecee7cfab17d623cda09c9
s7evinkelevra <kelevra.1337@gmail.com>
2022-05-25 19:01:28 +0200

    implement csv writer

---
+++ src/CSVWriter.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Jan on 25.05.2022.
+//
+
+#include "CSVWriter.h"
+
+#include <utility>
+
---
+++ src/CSVWriter.h
@@ -0,0 +1,50 @@
+//
+// Created by Jan on 25.05.2022.
+//
+
+#ifndef PERFTEST_CPP_CSVWRITER_H
+#define PERFTEST_CPP_CSVWRITER_H
+
+#include <string>
+#include <utility>
+#include <fstream>
+#include <iostream>
+
+class CSVWriter {
+private:
+    std::string _file_path;
+    std::string _delimiter;
+    int _line_count;
+    std::fstream _file;
+public:
+    explicit CSVWriter(std::string filepath, std::string delimiter = ",") : _file_path(std::move(filepath)), _delimiter(std::move(delimiter)), _line_count(0) {
+        // Open the file in truncate mode if first line else in Append Mode
+        _file.open(_file_path, std::ios::out | (_line_count ? std::ios::app : std::ios::trunc));
+    };
+
+    ~CSVWriter(){
+        // Close the file
+        _file.close();
+    }
+
+    template<typename T>
+    void addRow(T first, T last);
+
+};
+
+template<typename T>
+void CSVWriter::addRow(T first, T last) {
+    // Iterate over the range and add each lement to file seperated by delimeter.
+    for (; first != last; )
+    {
+        _file << *first;
+        if (++first != last)
+            _file << _delimiter;
+    }
+    _file << "\n";
+    _line_count++;
+
+}
+
+
+#endif //PERFTEST_CPP_CSVWRITER_H
