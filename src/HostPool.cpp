532037e0bb9d66ad6f2e2c413559bb16ab5b0626
s7evinkelevra <kelevra.1337@gmail.com>
2022-05-27 16:00:58 +0200

    fix allele distribution generation

--- src/HostPool.cpp
+++ src/HostPool.cpp
@@ -12,21 +12,35 @@ HostPool::HostPool() {
 }
 */
 
-std::vector<std::unordered_map<int, int>> HostPool::getAlleleDistributions(int locus_id) {
-    std::vector<std::unordered_map<int,int>> dist_vec;
+std::vector<std::vector<std::unordered_map<int, int>>> HostPool::getAlleleDistributionsPerSpeciesPerLocus() {
+    std::vector<std::vector<std::unordered_map<int,int>>> species_loci_dist_vec;
+    species_loci_dist_vec.reserve(hosts.size());
 
-    for(int species_id = 0; species_id < hosts.size(); species_id++){
-        std::unordered_map<int, int> species_dist = getAlleleDistribution(species_id, locus_id);
-        dist_vec.push_back(species_dist);
+    for(int species_i = 0; species_i < hosts.size(); species_i++){
+        std::vector<std::unordered_map<int,int>> species_dist_vec = getAlleleDistributionsPerLocus(species_i);
+        species_loci_dist_vec.emplace_back(species_dist_vec);
     }
 
-    return dist_vec;
+    return species_loci_dist_vec;
 }
 
-std::unordered_map<int, int> HostPool::getAlleleDistribution(int speciesId, int locus_id) {
+
+std::vector<std::unordered_map<int, int>> HostPool::getAlleleDistributionsPerLocus(int species_id) {
+    std::vector<std::unordered_map<int,int>> locus_dist_vec;
+    locus_dist_vec.reserve(max_loci_count);
+
+    for(int locus_i = 0; locus_i < max_loci_count; locus_i++){
+        std::unordered_map<int, int> locus_dist = getAlleleDistribution(species_id, locus_i);
+        locus_dist_vec.emplace_back(locus_dist);
+    }
+
+    return locus_dist_vec;
+}
+
+std::unordered_map<int, int> HostPool::getAlleleDistribution(int species_id, int locus_id) {
     std::unordered_map<int, int> dist;
 
-    for(auto& host : hosts[speciesId]){
+    for(auto& host : hosts[species_id]){
         if(host.chromosome_1_allele_ids.size() > locus_id){
             dist[host.chromosome_1_allele_ids[locus_id]]++;
         };
@@ -43,7 +57,7 @@ std::vector<int> HostPool::getAlleleCounts(int locus_id) {
     std::vector<int> counts;
     counts.reserve(hosts.size());
     for(int species_id = 0; species_id < hosts.size(); species_id++){
-        counts.push_back(getAlleleCount(species_id, locus_id));
+        counts.emplace_back(getAlleleCount(species_id, locus_id));
     }
     return counts;
 }
@@ -62,3 +76,42 @@ void HostPool::updateFitness() {
         fitness_sum[species_i] = current_fitness_sum;
     }
 }
+
+void HostPool::updateMaxLociCount() {
+    for(int species_i = 0; species_i < hosts.size(); species_i++){
+        for(auto& host : hosts[species_i]){
+            max_loci_count = std::max({(int)max_loci_count, (int)host.chromosome_1_allele_ids.size(), (int)host.chromosome_2_allele_ids.size()});
+        }
+    }
+}
+
+
+std::vector<std::unordered_map<int, int>> HostPool::getAlleleDistributionAcrossAllLociPerSpecies() {
+    std::vector<std::unordered_map<int,int>> species_dist_vec;
+    species_dist_vec.reserve(hosts.size());
+
+    for(int species_i = 0; species_i < hosts.size(); species_i++){
+        std::unordered_map<int, int> dist = getAlleleDistributionAcrossAllLoci(species_i);
+        species_dist_vec.emplace_back(dist);
+    }
+
+    return species_dist_vec;
+}
+
+
+std::unordered_map<int, int> HostPool::getAlleleDistributionAcrossAllLoci(int species_id) {
+    std::unordered_map<int, int> dist;
+
+    for(auto& host : hosts[species_id]){
+        for(int& id : host.chromosome_1_allele_ids){
+            dist[id]++;
+        }
+        for(int& id : host.chromosome_2_allele_ids){
+            dist[id]++;
+        }
+    }
+
+    return dist;
+}
+
+
--- src/HostPool.h
+++ src/HostPool.h
@@ -12,21 +12,45 @@
 
 class HostPool {
 public:
-/*    HostPool(int hostPoolSize);
-    virtual ~HostPool();*/
+
+    // return allele counts per locus
+    // e.g. host[0].chromosome_1 -> alleles [2,5]
+    // e.g. host[0].chromosome_2 -> alleles [5,5]
+    // dist for locus 0
+    // dist[2] = 1
+    // dist[5] = 1
+    // dist for locus 1
+    // dist[2] = 0
+    // dist[5] = 2
     std::vector<std::vector<Host>> hosts;
-    std::vector<std::unordered_map<int, int>> getAlleleDistributions(int locus_id);
-    std::unordered_map<int, int> getAlleleDistribution(int speciesId, int locus_id);
+    std::vector<std::vector<std::unordered_map<int, int>>> getAlleleDistributionsPerSpeciesPerLocus();
+    std::vector<std::unordered_map<int, int>> getAlleleDistributionsPerLocus(int species_id);
+    std::unordered_map<int, int> getAlleleDistribution(int species_id, int locus_id);
+
+    // returns allele counts ACROSS ALL LOCI
+    // e.g. host[0].chromosome_1 -> alleles [2,5]
+    // e.g. host[0].chromosome_2 -> alleles [5,5]
+    // distribution for both loci:
+    // dist[2] = 1
+    // dist[5] = 3
+    std::vector<std::unordered_map<int, int>> getAlleleDistributionAcrossAllLociPerSpecies();
+    std::unordered_map<int, int> getAlleleDistributionAcrossAllLoci(int species_id);
 
     std::vector<int> getAlleleCounts(int locus_id);
     int getAlleleCount(int speciesId, int locus_id);
 
+    // vector of the sums of fitness for each species
+    // updated in updateFitness
     std::vector<double> fitness_sum;
 
     // calculates all the fitness values for each host and saves it to that hosts fitness
     // also updates the fitness sum for that species
     void updateFitness();
 
+    // manually track the maximum number of loci found in an individual
+    int max_loci_count;
+    // check all individuals and set max loci count
+    void updateMaxLociCount();
 
 };
 
