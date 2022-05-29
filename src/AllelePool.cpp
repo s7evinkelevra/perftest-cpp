ca01933614e2a83d2004075c3440c8a0c9734ec6
s7evinkelevra <kelevra.1337@gmail.com>
2022-05-29 22:02:22 +0200

    start implementation on allele purge

--- src/AllelePool.cpp
+++ src/AllelePool.cpp
@@ -12,3 +12,16 @@ unsigned long AllelePool::addAllele(int species_id, int parent_id, std::string s
     alleles[species_id].push_back(Allele(parent_id, (int)alleleId,std::move(sequence)));
     return alleleId;
 }
+
+//TODO(JAN): completely untested
+//BUG(JAN): ALLELES RELY ON INDEX==ID, THIS BREAKS THAT!!! DO NOT USE UNTIL SOLUTION IS FOUND
+void AllelePool::purgeUnused(int species_id, std::unordered_map<int,int>& allele_dist){
+    std::vector<Allele> used_alleles;
+    used_alleles.reserve(allele_dist.size());
+
+    for(auto& item : allele_dist){
+        used_alleles.emplace_back(alleles[species_id][item.first]);
+    }
+
+    alleles[species_id] = used_alleles;
+}
--- src/AllelePool.h
+++ src/AllelePool.h
@@ -6,12 +6,16 @@
 #define PERFTEST_CPP_ALLELEPOOL_H
 
 #include <vector>
+#include <unordered_map>
 #include "Allele.h"
 
 class AllelePool {
 public:
+    std::vector<int> counts;
     std::vector<std::vector<Allele>> alleles;
     unsigned long addAllele(int species_id,int parent_id, std::string sequence);
+
+    void purgeUnused(int species_id, std::unordered_map<int, int>& allele_dist);
 };
 
 
