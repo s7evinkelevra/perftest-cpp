a76e142c418fae5d1a6a56937ed7635c3ee566ab
s7evinkelevra <kelevra.1337@gmail.com>
2022-06-03 20:37:40 +0200

    perf testing

--- tests.cpp
+++ tests.cpp
@@ -1,5 +1,14 @@
 #include <iostream>
+#include <string>
+#include "src/Helper.h"
 
 int main(int argc, char const *argv[]){
-  
+  std::string a = "tester";
+  std::string b = "testa";
+
+  for(int i = 0; i < 100000000; i++) {
+      Helper::LevenshteinDistance(a,b);
+  }
+
+
 }
