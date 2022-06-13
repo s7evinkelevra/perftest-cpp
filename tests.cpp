bb446a28cae5b5c4ff25633da1c7e0087afd558d
s7evinkelevra <kelevra.1337@gmail.com>
2022-06-13 23:15:08 +0200

    add timing

--- tests.cpp
+++ tests.cpp
@@ -1,14 +1,22 @@
 #include <iostream>
 #include <string>
+#include <chrono>
 #include "src/Helper.h"
 
 int main(int argc, char const *argv[]){
-  std::string a = "tester";
-  std::string b = "testa";
+    std::string a = "tester";
+    std::string b = "testa";
 
-  for(int i = 0; i < 100000000; i++) {
-      Helper::LevenshteinDistance(a,b);
-  }
+    auto start_time = std::chrono::steady_clock::now();
 
+    for(int i = 0; i < 100000000; i++) {
+        Helper::LevenshteinDistance(a,b);
+    }
+
+    auto end_time = std::chrono::steady_clock::now();
+
+    std::cout << "run time: "
+              << std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time).count()
+              << " ms" << std::endl;
 
 }
