5324b628d74bd3df49a1066adfb826e0a503155d
s7evinkelevra <kelevra.1337@gmail.com>
2022-06-13 23:29:20 +0200

    add multithreading

--- tests.cpp
+++ tests.cpp
@@ -1,14 +1,21 @@
 #include <iostream>
 #include <string>
 #include <chrono>
+#include <thread>
+#include <omp.h>
 #include "src/Helper.h"
 
 int main(int argc, char const *argv[]){
     std::string a = "tester";
     std::string b = "testa";
 
-    auto start_time = std::chrono::steady_clock::now();
+    unsigned int threads_available = std::thread::hardware_concurrency();
+    std::cout << "threads available: " << threads_available << "\n";
+    omp_set_num_threads(threads_available);
 
+
+    auto start_time = std::chrono::steady_clock::now();
+    #pragma omp parallel for default(none) shared(a, b)
     for(int i = 0; i < 100000000; i++) {
         Helper::LevenshteinDistance(a,b);
     }
